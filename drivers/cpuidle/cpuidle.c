1ee53689477c52f315a85109554501b741834ade
Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>
2020-04-01 09:03:17 +0000

    k

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -628,17 +628,8 @@ static void smp_callback(void *v)
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
-	struct cpumask cpus;
 
-	if (v)
-		cpumask_andnot(&cpus, v, cpu_isolated_mask);
-	else
-		cpumask_andnot(&cpus, cpu_online_mask, cpu_isolated_mask);
-
-	preempt_disable();
-	smp_call_function_many(&cpus, smp_callback, NULL, 1);
-	preempt_enable();
-
+	wake_up_all_idle_cpus();
 	return NOTIFY_OK;
 }
 
